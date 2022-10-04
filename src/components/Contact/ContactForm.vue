c4da7c396327d8470d86ace559d32dd78800fcd1
Samuel De la Cruz <s.delacruz@tele.finance>
2022-10-03 22:13:55 -0400

    feat: contact form component added

---
+++ src/components/Contact/ContactForm.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="w-full flex-col m-auto items-center flex mb-5">
+    <h2
+      class="
+        font-semibold
+        text-lg
+        md:text-3xl
+        lg:text-4xl
+        font-body
+        leading-relaxed
+        uppercase
+        mt-3
+      "
+    >
+      Contact our sales team
+    </h2>
+    <span class="text-sm italic text-gray-600">Write an email to house.store@hstore.com</span>
+    <form
+      @submit.prevent="handleSubmit"
+      class="flex p-4 mt-4 w-full md:w-1/2 flex-col"
+    >
+      <div 
+        v-show="showSuccessMessage"
+        class="
+          mt-3
+          mb-2
+          p-2
+          border border-gray-400
+          rounded-md
+          text-lg
+          bg-green-500
+          w-full
+          text-white
+        "
+      >
+        <span>Sent email successfully</span>
+      </div>
+      <fieldset class="w-full gap-3 flex justify-between">
+        <input
+          v-model="name"
+          required
+          type="text"
+          class="
+            w-full
+            outline-none
+            rounded-full
+            pr-12
+            pl-5
+            py-2
+            text-lg
+            border-2 border-gray-400
+          "
+          name="name"
+          id="name"
+          placeholder="Enter your name"
+        />
+        <input
+          required
+          v-model="email"
+          type="email"
+          name="email"
+          class="
+            w-full
+            outline-none
+            rounded-full
+            pr-12
+            pl-5
+            py-2
+            text-lg
+            border-2 border-gray-400
+          "
+          id="email"
+          placeholder="Enter a valid  email address"
+        />
+      </fieldset>
+      <textarea
+        name="message"
+        v-model="message"
+        required
+        class="
+          w-full
+          outline-none
+          rounded-md
+          mt-4
+          pr-12
+          pl-5
+          py-2
+          text-lg
+          border-2 border-gray-400
+        "
+        id="message"
+        cols="30"
+        rows="10"
+        placeholder="Enter your message"
+      ></textarea>
+      <button
+        class="
+          py-3
+          outline-none
+          bg-blue-400
+          hover:bg-blue-700
+          rounded-md
+          font-semibold
+          uppercase
+          mt-4
+          text-white
+        "
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+  
+  const SHOW_SUCCESS_MESSAGE_TIME = 3000;
+
+  export default {
+  name: "ContactForm",
+  data: () => ({
+    email: "",
+    name: "",
+    message: "",
+    showSuccessMessage: false,
+  }),
+  methods: {
+    handleSubmit() {
+      if (!this.email && !this.name && !this.message) return;
+      this.showSuccessMessage = true;
+
+      setTimeout(() => {
+        this.showSuccessMessage = false;
+      }, SHOW_SUCCESS_MESSAGE_TIME);
+    },
+  },
+};
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,73 +1,111 @@
 <template>
-<div>
-  <div class="bg-gray-300">
-   <breadcrumb  :items="itemsBreadcrumb" divider=">"/>
-  </div>
-  
-  <div class="text-center">
-    <h1 class="font-semibold text-2xl md:text-3xl lg:text-5xl 
-     font-display leading-relaxed uppercase mt-3">
-     contact us
-    </h1>
-    <p class="leading-loose text-xl font-body p-2 font-light">
-      contact us and make your house more elegant
-    </p>
-  </div>
-  
-  <div class="md:flex justify-around items-center flex-wrap my-10 mx-5"> 
-    <contact-details-card title="address" content="duarte street #101">
-      <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      fill="none" 
-      viewBox="0 0 24 24" 
-      class="text-white h-12 w-12"
-      stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-      </svg>
-    </contact-details-card>
-    
-    <contact-details-card title="email" content="house.store@hstore.com">
-      <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      fill="none" 
-      viewBox="0 0 24 24" 
-      class="text-white h-12 w-12"
-      stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-      </svg>
-    </contact-details-card>
+  <div>
+    <div class="bg-gray-300">
+      <breadcrumb :items="itemsBreadcrumb" divider=">" />
+    </div>
 
-    <contact-details-card title="phone number" content="111-111-1111">
-      <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      fill="none" 
-      viewBox="0 0 24 24" 
-      class="text-white h-12 w-12"
-      stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-      </svg>
-    </contact-details-card>
+    <div class="text-center mb-10">
+      <h1
+        class="
+          font-semibold
+          text-2xl
+          md:text-3xl
+          lg:text-5xl
+          font-display
+          leading-relaxed
+          uppercase
+          mt-3
+        "
+      >
+        contact us
+      </h1>
+      <p class="leading-loose text-xl font-body p-2 font-light">
+        contact us and make your house more elegant
+      </p>
+    </div>
+
+    <div class="md:flex justify-around items-center flex-wrap my-10 mx-5">
+      <contact-details-card title="address" content="duarte street #101">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="text-white h-12 w-12"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+      </contact-details-card>
+
+      <contact-details-card title="email" content="house.store@hstore.com">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="text-white h-12 w-12"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+          />
+        </svg>
+      </contact-details-card>
+
+      <contact-details-card title="phone number" content="111-111-1111">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="text-white h-12 w-12"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+          />
+        </svg>
+      </contact-details-card>
+    </div>
+
+    <hr class="mx-5 h-1 bg-gray-400 my-5" />
+    
+    <contact-form />
   </div>
-  
-  
-</div>
 </template>
 
 <script>
-import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
-import ContactDetailsCard from '@/components/Contact/ContactDetailsCard';
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
+import ContactDetailsCard from "@/components/Contact/ContactDetailsCard";
+import ContactForm from "@/components/Contact/ContactForm";
+
 export default {
-  name: 'contact',
+  name: "contact",
   components: {
     Breadcrumb,
-    ContactDetailsCard
+    ContactForm,
+    ContactDetailsCard,
   },
   data: () => ({
-  itemsBreadcrumb: [
-    {text: 'home', disabled: false, href: '/'},
-    {text: 'contact', disabled: true, href: '#'},
-  ]
-  })
-}
+    itemsBreadcrumb: [
+      { text: "home", disabled: false, href: "/" },
+      { text: "contact", disabled: true, href: "#" },
+    ],
+  }),
+};
 </script>
