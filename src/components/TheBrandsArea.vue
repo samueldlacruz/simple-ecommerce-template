6c8d05b1d17daacb1fd9a6a91a141f968b0770c1
Samuel De la Cruz <s.delacruz@tele.finance>
2022-10-03 22:12:53 -0400

    feat: the brand area component updated

--- src/components/TheBrandsArea.vue
+++ src/components/TheBrandsArea.vue
@@ -1,18 +1,51 @@
 <template>
- <div class="bg-white p-5">
-  <div class="grid gap-3 md:gap-10 md:grid-cols-4 grid-cols-2 md:grid-rows-1 grid-rows-2">
-    <div v-for="(brand, index) in brands" :key="`${brand.name}-${index}`" class="w-32 h-24 flex items-center">
-      <img class="w-full" :src="require(`@/assets/images/${brand.name}-logo.png`)" :alt="brand.name">
+  <div class="bg-white p-5 mb-5">
+    <h3
+      class="
+        md:text-4xl mb-4
+        text-3xl text-center
+        font-display font-bold
+        leading-loose
+      "
+    >
+      Brands
+    </h3>
+    <div
+      class="
+        grid
+        gap-3
+        md:gap-2 md:grid-cols-4
+        grid-cols-2
+        md:grid-rows-1
+        grid-rows-2
+      "
+    >
+      <div
+        v-for="(brand, index) in brands"
+        :key="`${brand.name}-${index}`"
+        class="w-full flex items-center"
+      >
+        <img
+          class="w-full h-20 object-contain"
+          :title="brand.name"
+          :src="require(`@/assets/images/${brand.name}-logo.png`)"
+          :alt="brand.name"
+        />
+      </div>
     </div>
   </div>
- </div>
 </template>
 
 <script>
 export default {
-  name:'TheBrandsArea',
-  data:() => ({
-    brands: [{name:'lg'}, {name:'samsung'}, {name:'whirlpool'},{name:'osterizer'}]
-  })
-}
+  name: "TheBrandsArea",
+  data: () => ({
+    brands: [
+      { name: "lg" },
+      { name: "whirlpool" },
+      { name: "samsung" },
+      { name: "osterizer" },
+    ],
+  }),
+};
 </script>
